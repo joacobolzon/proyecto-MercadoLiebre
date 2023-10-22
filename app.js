bdf3bbefca0387f8f295cf700e6af07c97d1a9db
joacobolzon <joaco.bolzon3@gmail.com>
2023-10-22 03:56:07 -0300

    publicfolder

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 const PORT = 3001;
 
+const publicFolderPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicFolderPath));
+
 app.listen(PORT, () => {
   console.log("Server running in port " + PORT);
 });
