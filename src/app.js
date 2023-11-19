9466eeb30e8e120906e9e9ecce477d5c8f3b302a
joacobolzon <joaco.bolzon3@gmail.com>
2023-11-19 04:37:16 -0300

    error implemented

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const methodOverride = require("method-override");
 const path = require("path");
 const app = express();
 const PORT = 3001;
+
 //Routes
 const routesMain = require("./routes/main");
 const routesProducts = require("./routes/products");
@@ -16,7 +18,14 @@ app.listen(PORT, () => {
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.use("/users", usersRoutes);
 app.use("/", routesMain);
+
+app.use((req,res,next) => {
+  res.status(404).render('error')
+})
 //app.use("/products", routesProducts);
